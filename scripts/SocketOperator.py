6651f21dbc7060a6357c0f83dd87211a2ba149fb
Pulkit Juneja <pulkit.16296@gmail.com>
2017-04-27 19:40:25 +0530

    socket funtionality working

--- scripts/SocketOperator.py
+++ scripts/SocketOperator.py
@@ -1,35 +1,52 @@
 import bpy
 import socket
+import os
+import sys
+
+absolutePath = "/Users/pulkitjuneja/Documents/projects/EVA/scripts"
+sys.path.append(absolutePath)
+
+from animationController import main
 
 
 class socketModal(bpy.types.Operator):
-  bl_idname = "object.modal_operator"
-  bl_label = "Lip synch Operator"
+    bl_idname = "object.modal_operator"
+    bl_label = "Lip synch Operator"
+    _timer = None
 
-  def __init__(self):
+    def __init__(self):
         print("Listen Start")
+        os.chdir(absolutePath)
         self.port = 1301
 
-  def __del__(self):
+    def __del__(self):
         print("Listen End")
 
-  def invoke(self, contect, event):
-      self.execute(context)
-      self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      self.socket.bind(("127.0.0.1", self.port))
-      print('socket listening on port', port)
-      return {'RUNNING_MODAL'}
+    def execute(self, context):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setblocking(0)
+        self.socket.bind(("127.0.0.1", self.port))
+        print('socket listening on port', self.port)
+        self._timer = context.window_manager.event_timer_add(1.0, context.window)
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
 
-  def execute(self, context):
-      print("execute function called")
-      return {'FINISHED'}
+    def modal(self, context, event):
 
-  def modal (self, context, event):
-      data,addr = self.socket.recvfrom (1024*1024)
-      print data
-      if event.type == 'BACK_SLASH':
-        return {'FINISHED'}
+        if event.type == "TIMER":
+            data = None
+            try:
+                data = self.socket.recv(1024 * 1024)
+            except socket.error:
+                print ("no data") 
+                return {"RUNNING_MODAL"}
+            print(data)
+        if event.type == 'BACK_SLASH':
+            self.socket.close()
+            return {'FINISHED'}
+        return {'PASS_THROUGH'}
 
 
+bpy.utils.register_class(socketModal)
 
-bpy.utils.register_class (socketModal)        
+import bpy
---
+++ scripts/animationController.py
@@ -0,0 +1,89 @@
+import bpy
+import json
+
+shapeKeyNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'X']
+dataPathPrefix = "key_blocks[\""
+dataPathSuffix = "\"].value"
+
+
+def getShapeKeyAnimationInfo(mouthCue, fps):
+    frames = (float(mouthCue['end']) - float(mouthCue['start'])) * fps
+    frames = round(frames)
+    # print(frames)
+    phoneme = mouthCue['value']
+    for name in shapeKeyNames:
+        if phoneme in name:
+            return (frames, name)
+
+
+def addFaceShapeKeyFrame(shapeKeyName, previousKeyName, startFrame, frameCount):
+    faceShapeKeyParent = bpy.data.meshes['face'].shape_keys
+    dataPath = dataPathPrefix + shapeKeyName + dataPathSuffix
+    faceShapeKeyParent.key_blocks[shapeKeyName].value = 0.0
+    faceShapeKeyParent.keyframe_insert(dataPath, frame=startFrame)
+    faceShapeKeyParent.key_blocks[shapeKeyName].value = 1.0
+    finalFrame = startFrame + frameCount
+    if previousKeyName != None and previousKeyName != shapeKeyName:
+        dataPathPrevious = dataPathPrefix + previousKeyName + dataPathSuffix
+        faceShapeKeyParent.key_blocks[previousKeyName].value = 0.0
+        faceShapeKeyParent.keyframe_insert(dataPathPrevious, frame=finalFrame)
+    faceShapeKeyParent.keyframe_insert(dataPath, frame=finalFrame)
+
+
+def addJawShapeKeyFrame(shapeKeyName, previousKeyName, startFrame, frameCount):
+    jawShapeKeyParent = bpy.data.meshes['jaw'].shape_keys
+    jawKeyName = getJawKeyName(shapeKeyName)
+    dataPath = dataPathPrefix + jawKeyName + dataPathSuffix
+    previousJawKeyName = None
+    if previousKeyName:
+        previousJawKeyName = getJawKeyName(previousKeyName)
+        dataPathPrev = dataPathPrefix + previousJawKeyName + dataPathSuffix
+    finalFrame = startFrame + frameCount
+    print (jawKeyName , previousJawKeyName)
+    if previousJawKeyName != None and previousJawKeyName != jawKeyName:
+        jawShapeKeyParent.key_blocks[jawKeyName].value = 0.0
+        jawShapeKeyParent.keyframe_insert(dataPath, frame=startFrame)
+        jawShapeKeyParent.key_blocks[previousJawKeyName].value = 0.0
+        jawShapeKeyParent.keyframe_insert(dataPathPrev, frame=finalFrame)
+    jawShapeKeyParent.key_blocks[jawKeyName].value = 1.0
+    jawShapeKeyParent.keyframe_insert(dataPath, frame=finalFrame)
+
+
+def getJawKeyName(shapeKeyName):
+    if shapeKeyName in ['D', 'H']:
+        return 'W'
+    if shapeKeyName in ['C', 'E', 'F', 'G']:
+        return 'M'
+    if shapeKeyName in ['X', 'A', 'B']:
+        return 'C'
+
+
+def main(context):
+    scene = context.scene
+    scene.render.fps = 60
+    fps = scene.render.fps
+    clearAllAnimation()
+    with open('Documents/projects/EVA/scripts/phoneme.json') as myfile:
+        data = myfile.read().replace('\n', '')
+    phonemes = json.loads(data)
+    mouthCues = phonemes['mouthCues']
+    previousKey = None
+    framecounter = 1
+    for x in mouthCues:
+        animationData = getShapeKeyAnimationInfo(x, fps)
+        addFaceShapeKeyFrame(animationData[1], previousKey, framecounter, animationData[0])
+        addJawShapeKeyFrame(animationData[1], previousKey, framecounter, animationData[0])
+        previousKey = animationData[1]
+        framecounter += animationData[0]
+    scene.frame_set(1)
+    scene.frame_end = framecounter + 1
+    bpy.ops.screen.animation_play()
+
+
+def clearAllAnimation():
+    faceShapeKeyParent = bpy.data.meshes['face'].shape_keys
+    faceShapeKeyParent.animation_data_clear()
+
+
+if (__name__ == "__main__"):
+    main()
