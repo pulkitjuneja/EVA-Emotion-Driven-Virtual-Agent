293d0321d4a8e43f9b954474cad8262aeb5aeb79
Pulkit Juneja <pulkit.16296@gmail.com>
2017-05-02 02:00:08 +0530

    end animation operator

--- scripts/EndAnimationOperator.py
+++ scripts/EndAnimationOperator.py
@@ -1,44 +1,18 @@
-class ModalTimerOperator(bpy.types.Operator):
-    """Operator which runs its self from a timer"""
-    bl_idname = "wm.modal_timer_operator"
-    bl_label = "Modal Timer Operator"
+import bpy 
+import sys 
 
+
+class EndAnimationOperator(bpy.types.Operator):
+
+    bl_idname = "object.End_Animation_Operator"
+    bl_label = "End Animation Operator"
     _timer = None
+    
 
-    def modal(self, context, event):
-        if event.type in {'RIGHTMOUSE', 'ESC'}:
-            self.cancel(context)
-            return {'CANCELLED'}
+    def __init__(self):
+        self.port = 1301
 
-        if event.type == 'TIMER':
-            # change theme color, silly!
-            color = context.user_preferences.themes[0].view_3d.space.gradients.high_gradient
-            color.s = 1.0
-            color.h += 0.01
+    def __del__(self):
+        print("Listen End")
 
-        return {'PASS_THROUGH'}
-
-    def execute(self, context):
-        wm = context.window_manager
-        self._timer = wm.event_timer_add(0.1, context.window)
-        wm.modal_handler_add(self)
-        return {'RUNNING_MODAL'}
-
-    def cancel(self, context):
-        wm = context.window_manager
-        wm.event_timer_remove(self._timer)
-
-
-def register():
-    bpy.utils.register_class(ModalTimerOperator)
-
-
-def unregister():
-    bpy.utils.unregister_class(ModalTimerOperator)
-
-
-if __name__ == "__main__":
-    register()
-
-    # test call
-    bpy.ops.wm.modal_timer_operator()
+    def execute (self, context):
